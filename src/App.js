dcc082f7f29baf7c73cf35cdd8dc752904135015
DeepanshuGarg <deepanshugarg2812@gmail.com>
2020-05-23 17:20:52 +0530

    Login Component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import React from 'react';
 import './App.css';
-import AddItem from './components/AddItem';
 
 class App extends React.Component{
   render(){
     return(
       <>
-      <div className="mainFrame">
-        <AddItem />
-      </div>
+      
       </>
     );
   }  
